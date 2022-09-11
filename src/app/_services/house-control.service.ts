8d43140dc054183dca1c68df059aaae16ce47b4f
Moni Waterhouse <monica.waterhouse@estudiantec.cr>
2022-09-11 16:30:46 -0600

    [Create] light cards and requests for light control

--- src/app/_services/house-control.service.ts
+++ src/app/_services/house-control.service.ts
@@ -8,11 +8,40 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { LoginInfo, RegisterInfo } from '../_interfaces/user';
 import { ControlLight, LightState,  DoorState} from '../_interfaces/house-state';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HouseControlService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+  
+  }
+
+  getLightStatus(lightState : LightState){
+    return this.http.post(`${environment.apiUrl}/get_light_state`, lightState);
+
+  }
+
+  changeLightState(controlLight : ControlLight){
+    return this.http.post(`${environment.apiUrl}/change_light_state`, controlLight);
+  }
+
+  getDoorState(doorState : DoorState){
+    return this.http.post(`${environment.apiUrl}/get_door_state`, doorState);
+  }
+
+  getGardenPhoto(){
+    return this.http.get(`${environment.apiUrl}/take_photo`);
+  }
+
+  loginUser(loginInfo : LoginInfo){
+    return this.http.post(`${environment.apiUrl}/login`, loginInfo);
+  }
+
+  registerUser(registerInfo : RegisterInfo){
+    return this.http.post(`${environment.apiUrl}/sign_up`, registerInfo);
+
+  }
 }
--- src/app/lights/lights.component.ts
+++ src/app/lights/lights.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthGuardService } from '../_services/auth-guard.service';
 import { HouseControlService } from '../_services/house-control.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lights',
@@ -9,9 +10,46 @@ import { HouseControlService } from '../_services/house-control.service';
 })
 export class LightsComponent implements OnInit {
 
-  constructor(private auth : AuthGuardService, private houseControlSrv : HouseControlService) { }
+  room !: string;
+  livingRoomStatus !: number;
+  dinningRoomStatus !: number;
+  kitchenStatus !: number;
+  bedroom1Status !: number;
+  bedroom2Status !: number;
+
+  constructor(private auth : AuthGuardService, private houseControlSrv : HouseControlService, private route : Router) { }
 
   ngOnInit(): void {
+  }
+
+  lightLivingRoom(){
+    this.room = "living-room";
+    console.log(this.room);
+
+  }
+
+  lightDinningRoom(){
+    this.room = "dinning-room";
+    console.log(this.room);
+
+  }
+
+  lightKitchen(){
+    this.room = "kitchen";
+    console.log(this.room);
+
+  }
+
+  lightBedroom1(){
+    this.room = "bedroom1";
+    console.log(this.room);
+
+  }
+
+  lightBedroom2(){
+    this.room = "bedroom2";
+    console.log(this.room);
+
   }
 
 }
